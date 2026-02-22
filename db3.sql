7afa393a9ce4188d1a6ede56ee1f8ca736863f56
suhasa <ubuntu@Suhasa.localdomain>
2026-02-22 15:09:56 -0700

    fix files

--- db3.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE quiz_history (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    score INT NOT NULL,
-    total_questions INT NOT NULL,
-    taken_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
